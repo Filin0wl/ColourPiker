9994cd5d9c782f11dda72f1760ccdec23fbac228
Filin <nastia-dogf@mail.ru>
2022-07-21 15:19:10 +0300

    Fixed design

--- ColourPiker/ViewControllers/EditColourViewController.swift
+++ ColourPiker/ViewControllers/EditColourViewController.swift
@@ -15,6 +15,7 @@ class EditColourViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
         // Do any additional setup after loading the view.
     }
 
